15392935400490784383665f2bd0d32c0522d0d8
Seid <101628221+seid1zahidov@users.noreply.github.com>
2022-07-05 00:51:52 +0400

    meartnjs1hom

---
+++ jsc/main.js
@@ -0,0 +1,16 @@
+// task 1
+var hour  = new Date().getHours();
+if(hour > 20 && hour < 22){
+    alert('axshaminx xeir 1')
+}
+else if (hour >22 && hour > 23) {
+    alert('axshamin xeir 2')
+}
+else if (hour >23 && hour > 24) {
+    alert('Axshamin Xeir3')
+}
+else if(hour => 24 && hour > 10){
+    alert('sabahin xeir')
+}
+
+
